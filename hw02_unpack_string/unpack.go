dda71ad38aeac8aec46b32b5ab1075192e8794e3
georgiiagon <georgiiagon@gmail.com>
2021-09-05 16:07:24 +0300

    done first part of hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(initStr string) (string, error) {
+	byteStr := []byte(initStr)
+	var tmpLetter byte = 0
+	var resultString []byte
+	for _, letter := range byteStr {
+		if letter >= 'a' && letter <= 'z' {
+			tmpLetter = letter
+			resultString = append(resultString, letter)
+			continue
+		}
+
+		if tmpLetter == 0 {
+			return "", ErrInvalidString
+		}
+
+		if letter >= '1' && letter <= '9' {
+			repeatTimes, _ := strconv.Atoi(string(letter))
+			for i := repeatTimes; i > 1; i-- {
+				resultString = append(resultString, tmpLetter)
+			}
+			tmpLetter = 0
+			continue
+		}
+
+		if letter == '0' {
+			resultString = resultString[:len(resultString)-1]
+		}
+	}
+
+	return string(resultString), nil
 }
