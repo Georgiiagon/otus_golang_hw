ccc4397b5a206f760d2c8c359382b02bda9dce84
georgiiagon <georgiiagon@gmail.com>
2021-09-25 13:20:06 +0300

    done with linked list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,72 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int
+	front *ListItem
+	back  *ListItem
+}
+
+func (l list) Len() int {
+	return l.len
+}
+
+func (l list) Front() *ListItem {
+	return l.front
+}
+
+func (l list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{Value: v, Next: l.Front()}
+	if l.Front() != nil {
+		l.Front().Prev = item
+	}
+	if l.Back() == nil {
+		l.back = item
+	}
+	l.front = item
+	l.len++
+
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{Value: v, Prev: l.Back()}
+	if l.Back() != nil {
+		l.Back().Next = item
+	}
+	if l.Front() == nil {
+		l.front = item
+	}
+	l.back = item
+	l.len++
+
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	if l.front == i {
+		l.front = i.Next
+	}
+	if l.back == i {
+		l.back = i.Prev
+	}
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -22,7 +22,6 @@ func TestList(t *testing.T) {
 		l.PushBack(20)  // [10, 20]
 		l.PushBack(30)  // [10, 20, 30]
 		require.Equal(t, 3, l.Len())
-
 		middle := l.Front().Next // 20
 		l.Remove(middle)         // [10, 30]
 		require.Equal(t, 2, l.Len())
