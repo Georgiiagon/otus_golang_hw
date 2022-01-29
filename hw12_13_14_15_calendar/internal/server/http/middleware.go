87d5eb05a9f81fc543010716102f8bdbe1deb231
georgiiagon <georgiiagon@gmail.com>
2022-01-29 10:23:18 +0300

    add logger to middleware

--- hw12_13_14_15_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware.go
@@ -1,12 +1,23 @@
 package internalhttp
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 func (s *Server) loggingMiddleware(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		s.Logger.Info("hello there!")
+		s.Logger.Info(fmt.Sprintf("%s [%s] %s %s %s %v %v %s",
+			r.RemoteAddr,
+			time.Now().Format("02/Jan/2006:15:04:05 -0700"),
+			r.Method,
+			r.URL,
+			r.Proto,
+			200,
+			r.ContentLength,
+			r.UserAgent()))
+
 		h(w, r)
 	}
 }
